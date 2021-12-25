9ad83bff1d10d1c5f39b605118b2d69b326c9fe6
Upauper63 <upauper63@gmail.com>
2021-12-25 12:12:27 +0900

    fix

--- breaking_news_python_bot.py
+++ breaking_news_python_bot.py
@@ -11,7 +11,6 @@ with open('settings.yaml') as f:
 today_str = datetime.date.today().strftime('%Y-%m-%d')[:10]
 yesterday_str = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')[:10]
 url = "https://qiita.com/api/v2/items?per_page=100&query=tag:Python%20created:%3E=" + yesterday_str + "%20created:%3C" + today_str
-print(url)
 res = urllib.request.urlopen(url)
 entries = json.loads(res.read().decode('utf8'))
 for entry in entries:
